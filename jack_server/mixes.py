d6f4546fe8a81dec74edf1a8a53a958312b3aee3
Mitchell Woodbine <mitchellwoodbine@gmail.com>
2019-01-30 20:05:29 -0500

    Added more to osc section of mixes

--- jack_server/mixes.py
+++ jack_server/mixes.py
@@ -28,7 +28,12 @@ class JackHelp:
 
 class Mixes:
     # people = { <name>: [<headphone port left>,<headphone port right>],.. }
-    # inputs = { <name>: {"port":<audio_port>,"pan"<L,R,C>,"record":<True,False>,"global_mute":<True,False(default)>},.. }
+    # inputs = { <name>: {
+    #     "port":        <audio_port> OR [<audio_port>,<audio_port>],
+    #     "pan":         <L,R,C>,
+    #     "record":      <True,False>,
+    #     "global_mute": <True,False(default)>,
+    # },.. }
     # osc_start_port   = to interact with jackminimix instances, they each need their own port
     # osc_receive_port = to send solo and mute data out
     def __init__(self, people, inputs, osc_start_port=4000, osc_ip='127.0.0.1', osc_receive_port=3002):
@@ -41,13 +46,17 @@ class Mixes:
         self.ip         = osc_ip
         self.out_port   = osc_receive_port
 
+        # set position values for inputs, this is needed by jackminimix
+        for i, name in enumerate(inputs, 1):
+            self.inputs[name]["pos"] = i
+
         for person in people:
             # for solo to work, person and input must have matching names
             self.solos[person] = False
             result = { "outports": people[person] }
 
-            for i, name in enumerate(inputs):
-                inp = inputs[name]
+            for i, name in enumerate(self.inputs):
+                inp = self.inputs[name]
 
                 # global mute is for talkback but could be for anything
                 #  if global mute, add to self.mutes
@@ -62,7 +71,6 @@ class Mixes:
                     result[name] = {
                         "vol":  0,
                         "mute": mute,
-                        "pos":  i,      # needed to tell jackminimix which channel to modify
                     }
 
             self.mixes[person] = result
@@ -77,32 +85,46 @@ class Mixes:
             mix_left_out  = f"-l {mix['outports'][0]}"
             mix_right_out = f"-r {mix['outports'][1]}"
 
+            # start up instance of jackminimix
             self.mixes[name]["osc"] = udp_client.SimpleUDPClient(self.ip, port)
             os.system(f"jackminimix {num_inputs} {mix_name} {mix_port} {mix_left_out} {mix_right_out}")
+
             time.sleep(1) # give jackminimix a second to load
+
+            # make all the jack connections
             for inp in mix:
+                pos    = self.inputs[inp]["pos"]
+                pan    = self.inputs[inp]["pan"]
+                inport = self.inputs[inp]["port"]
+                if type(inport) is list:
+                    for p in inport:
+                        if pan in ["L","C"]: JackHelp.connect_port(p,f"{name}_mix:in{pos}_left")
+                        if pan in ["R","C"]: JackHelp.connect_port(p,f"{name}_mix:in{pos}_right")
+                else:
+                    if pan in ["L","C"]: JackHelp.connect_port(inport,f"{name}_mix:in{pos}_left")
+                    if pan in ["R","C"]: JackHelp.connect_port(inport,f"{name}_mix:in{pos}_right")
 
             port += 1
 
     def process_osc(self, path, value):
         path_sp = path[1:].split('/')
 
-        # This only processes osc prefaced by 'mixer'
+        # This only processes osc that are prefaced by 'mixer'
         if path_sp[0] != "mixer": return None
 
         # remove 'mixer' from path
         path_sp.pop(0)
 
-        if path_sp[0] == "vol":
-            kind, name, inp = path_sp
-            pos = self.mixes[name][inp]["pos"]
+        kind, name, inp = path_sp
+        pos = self.inputs[inp]["pos"]
 
+        if kind == "vol":
             # value from rails comes in 0.0->1.0 so
             #  this gives +10/-30 db for the sliders
             value = int((value * 40) - 30)
 
             # we want an accurate value from slider irregardless of mute/solo
-            self.mixes[name][inp] = value
+            self.mixes[name][inp][kind] = value
 
             # solo mutes your instrument for everyone but yourself
             if name != inp and self.solos.get(inp,False):
@@ -117,10 +139,18 @@ class Mixes:
 
             self.mixes[name]["osc"].send_message("/mixer/channel/set_gain", [pos, value])
 
-        elif path_sp[0] == "mute":
-            pass
-        elif path_sp[0] == "solo":
+        elif kind == "mute":
+            if value >= .5: value = True
+            else:           value = False
+            self.mixes[name][inp][kind] = value
+
+            if value: value = self.mixes[name][inp]["vol"]
+            else:     value = -90
+
+            self.mixes[name]["osc"].send_message("/mixer/channel/set_gain", [pos, value])
+
+        elif kind == "solo":
             pass
         else:
-            print "####> Error processing osc in mixer"
+            print("####> Error processing osc in mixer")
 
