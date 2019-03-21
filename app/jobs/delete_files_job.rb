92db445407835e3c58074f0fc89245e4f5a325db
Mitchell Woodbine <mitchellwoodbine@gmail.com>
2019-03-21 19:31:04 -0400

    Added verbose to delete_files_job.rb

--- app/jobs/delete_files_job.rb
+++ app/jobs/delete_files_job.rb
@@ -2,12 +2,16 @@ class DeleteFilesJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
+    puts "----> Deleting recorded files.. "
+
     folder = Time.now.strftime("%Y%m%d")
     files  = Dir["recorded/*"]
 
     if not files.empty?
       cmd = `mkdir -p recorded_bup/#{folder} && mv recorded/* recorded_bup/#{folder}/.`
     end
+
+    puts "----> Deletion Complete!"
   end
 
 end
