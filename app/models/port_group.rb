3160fccd2175e55baae2ca64a5ab7eddc2486256
Mitchell Woodbine <mitchellwoodbine@gmail.com>
2019-06-09 11:23:45 -0400

    Delete associated chans when deleting port groups

--- app/models/port_group.rb
+++ app/models/port_group.rb
@@ -1,4 +1,14 @@
 class PortGroup < ApplicationRecord
   belongs_to :user, optional: true
   has_many :ports
+  before_destroy :clean_channels
+
+  private
+
+  def clean_channels
+    Channel.where(port_group: self).each do |channel|
+      channel.destroy
+    end
+  end
+
 end
