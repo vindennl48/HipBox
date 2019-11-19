833673dafcce067b3b177490798befc14f2221e4
Mitchell Woodbine <mitchellwoodbine@gmail.com>
2019-11-19 07:26:29 -0500

    Added => to Cmd::Debug output
    This carrot at the beginning of the message output is much easier
    to spot in the long list of console output.

--- app/models/cmd/debug.rb
+++ app/models/cmd/debug.rb
@@ -3,7 +3,7 @@ class Cmd::Debug
     method_name = caller(1,1)[0][/`.*'/].tr("`'","")
     class_name  = self.name
     title = "#{class_name}.#{method_name}".ljust(20).truncate(20, omission: '')
-    puts "  #{title} > #{message}"
+    puts "=> #{title} > #{message}"
   end
 end
 
