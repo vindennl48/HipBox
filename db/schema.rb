d6d1f128d7ca808884a542dc94f0e8594b51bf41
Mitchell Woodbine <mitchellwoodbine@gmail.com>
2019-11-27 14:44:47 -0500

    Updated Schema to include 'is_locked' to Clips

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_15_061555) do
+ActiveRecord::Schema.define(version: 2019_11_15_062012) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 2019_11_15_061555) do
     t.bigint "end", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "is_locked"
   end
 
   create_table "port_groups", force: :cascade do |t|
